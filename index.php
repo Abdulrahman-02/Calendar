fa4fa5ede017292123795085fcbce095e6cfda28
abdulrahman-02 <abderrahmanebenaissa740@gmail.com>
2021-12-16 17:50:44 +0100

    added project directories

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Calendrier</title>
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/script.js"></script>  
+  </head>
+  <body>
+    <div class="topnav">
+        <div class="login-container">
+          <form action="/action_page.php">
+            Connexion<input type="text" placeholder="Nom Utilisateur" name="username">
+            <input type="text" placeholder="Mot de passe" name="psw">
+            <button type="submit">S'identifier</button>
+            <p class="login-register-text">Vous n'avez pas un compte? <a href="#">Inscrivez-vous ici</a>.</p>
+          </form>
+        </div>
+      </div>
+      <!--calendar-->	
+  </body>
+</html>
