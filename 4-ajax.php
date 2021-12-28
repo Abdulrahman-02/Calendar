fe0c322ca92e21ee8103a0f1a4518ef461dec1d2
Abdulrahman-02 <abderrahmanebenaissa740@gmaiL.com>
2021-12-28 14:40:37 +0100

    added jours fériers

--- 4-ajax.php
+++ 4-ajax.php
@@ -64,9 +64,50 @@ require "2-cal-core.php";
     for ($day = 1; $day <= $Num_days_in_month; $day++) {
     $date = "{$_POST["year"]}-{$_POST["month"]}-$day";
     $weekend_test = (new DateTime($date))->format("w");
+    $jF= (new DateTime($date))->format("d");
+    $mF= (new DateTime($date))->format("m");
+    $an = (new DateTime($date))->format("Y");
+    
+    $G = ($an%19);
+    $C = floor($an/100);
+    $H = ($C - floor($C/4) - floor((8*$C+13)/25) + 19*$G + 15)%30;
+    $I = $H - floor($H/28)*(1 - floor($H/28)*floor(29/($H + 1))*floor((21 - $G)/11));
+    $J = ($an*1 + floor($an/4) + $I + 2 - $C + floor($C/4))%7;
+    $L = ($I - $J);
+    $MoisPaques = 3 + floor(($L + 40)/44);
+    $JoursPaques = $L + 28 - 31*floor($MoisPaques/4);
+    
+    $dateLp = date_create("$an-$MoisPaques-$JoursPaques");
+    $interval1 = new DateInterval('P1D');
+    $dateLp->add($interval1);
+    $lpJ = strval($dateLp->format('d'));
+    $lpM = strval($dateLp->format('m'));
+
+    $dateA = date_create("$an-$MoisPaques-$JoursPaques");
+    $interval2 = new DateInterval('P39D');
+    $dateA->add($interval2);
+    $aJ = strval($dateA->format('d'));
+    $aM = strval($dateA->format('m'));
+
+    $dateLpen = date_create("$an-$MoisPaques-$JoursPaques");
+    $interval3 = new DateInterval('P50D');
+    $dateLpen->add($interval3);
+    $lpenJ = strval($dateLpen->format('d'));
+    $lpenM = strval($dateLpen->format('m'));
+
      ?>
-    <div class="space day <?=$day==$nowDay?" today":""; 
-    ?><?=(($weekend_test==6)||($weekend_test==0))?" weak":"" ;?>
+    <div class="space day <?=$day==$nowDay?" today":""; ?>
+    <?=(($weekend_test=='6')||($weekend_test=='0'))?" weak":""; //weekend?>
+    <?=(($jF=='01') and ($mF=='01'))?"ferie":"";//jourAn?>
+    <?=((($jF=='01') or ($jF=='08')) and ($mF=='05'))?"ferie":"";//1 et 8 mai?>
+    <?=(($jF=='14') and ($mF=='07'))?"ferie":"";//fete national?>
+    <?=((($jF=='01') or ($jF=='11')) and ($mF=='11'))?"relg":"";//touissant et armistice?>
+    <?=(($jF=='25') and ($mF=='12'))?"relg":"";//noel?>
+    <?=(($jF=='15') and ($mF=='08'))?"relg":"";//assomption?>
+    <?=(($jF==$lpJ) and ($mF==$lpM))?"relg":"";//lundiPaque?>
+    <?=(($jF== $aJ) and ($mF==$aM))?"relg":"";//Ascension?>
+    <?=(($jF== $lpenJ) and ($mF==$lpenM))?"relg":"";//LundiPentecote?>
+
     " data-day="<?=$day?>">
       <div class="calnum"><?=$day?></div>
 
@@ -94,7 +135,6 @@ require "2-cal-core.php";
     for ($i=0; $i<$space; $i++) { 
       echo "<div class='space blank'></div>"; }
     
-
-// FIN DU CODE AJAX !
+      // FIN DU CODE AJAX !
 }
 
